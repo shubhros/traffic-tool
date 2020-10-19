d2c5832d126c329abb83cc09ff46b505661cfeec
shubhros <shubhro2@yahoo.co.in>
2020-10-19 10:26:28 +0530

    Handle multiple clients

--- server.go
+++ server.go
@@ -6,6 +6,8 @@ import (
 	"net"
 )
 
+var prevSeqNum map[uint64]uint32
+
 func startServer(port string) {
 	udpAddr, err := net.ResolveUDPAddr("udp4", ":"+port)
 	if err != nil {
@@ -17,21 +19,30 @@ func startServer(port string) {
 		panic("error listening")
 	}
 
+	prevSeqNum  = make(map[uint64]uint32)
+
 	fmt.Println("Listening on", udpAddr)
 
-	var seqNo int64
-	var prevSeqNo int64 = -1
+	var number uint64
+	var seqNo uint32
+	var prevSeqNo uint32
+	var ok bool
 	for {
-		err := binary.Read(ln, binary.LittleEndian, &seqNo)
+
+		err = binary.Read(ln, binary.BigEndian, &number)
 		if err != nil {
 			panic(err)
 		}
-		if seqNo != prevSeqNo+1 {
-			fmt.Printf("[srvr] Expecting sequence %x received sequence %x\n",
-				prevSeqNo+1, seqNo)
-		}
-		fmt.Printf("Received sequence number: %d\n", seqNo)
-		prevSeqNo = seqNo
+		id := number & 0x00000000ffffffff;
+		seqNo = uint32(number >> 32);
 
+		fmt.Println("id: %x, seq: %x\n", id, seqNo)
+		if prevSeqNo, ok = prevSeqNum[id]; ok {
+			if seqNo != prevSeqNo+1 {
+				fmt.Printf("[srvr] Expecting sequence %x received sequence %x cliend id: %x(%d)\n",
+					prevSeqNo+1, seqNo, id, int32(seqNo - prevSeqNo))
+			}
+		}
+		prevSeqNum[id] = seqNo
 	}
 }
